e32bdfbb15a2cb2e7583dec6aa0f09e63bd5a0f5
Sam Holtzman <sam.wolf.holtzman@gmail.com>
2024-02-10 21:47:29 -0800

    game board created

--- GUI.java
+++ GUI.java
@@ -1,3 +1,46 @@
-public class GUI {
-    
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class GUI extends JFrame {
+
+    static final int GRID_SIZE = 20;
+    static final int CELL_SIZE = 20;
+
+    GUI() {
+        setTitle("The Game of Life");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel gamePanel = new JPanel();
+        gamePanel.setLayout(new GridLayout(GRID_SIZE, GRID_SIZE));
+
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                JPanel cellPanel = new JPanel();
+                cellPanel.setPreferredSize(new Dimension(CELL_SIZE, CELL_SIZE));
+                cellPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+                gamePanel.add(cellPanel);
+            }
+        }
+
+        add(gamePanel);
+        pack();
+
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenWidth = (int) screenSize.getWidth();
+        int screenHeight = (int) screenSize.getHeight();
+        int frameWidth = (int) (screenWidth * (3.0/4.0));
+        int frameHeight = (int) (screenWidth * (3.0/4.0));
+        setSize(frameWidth, frameHeight);
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new GUI());
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
+
+
 public class Main {
     
+    GUI gui;
+
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+    }
 }
